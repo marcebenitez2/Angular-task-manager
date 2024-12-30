a97a705cb89ef7afc91e7f114effd2d7b1a3c784
marcebenitez2 <marcebenitez0607@gmail.com>
2024-12-30 00:34:32 -0300

    chore: instalacion de tailwind, entre otros

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./src/**/*.{html,ts}"], // Asegúrate de que esta ruta incluye tus archivos
+  theme: {},
+  plugins: [],
+};
